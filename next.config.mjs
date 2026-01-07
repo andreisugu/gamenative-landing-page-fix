b144c0d6bebe38692df6e5a5a112725bc8843f69
Andrei Șugubete <35407471+andreisugu@users.noreply.github.com>
2026-01-07 20:47:27 +0200

    github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const isGhPages = false
+const isGhPages = true
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: isGhPages ? '/gamenative-landing-page' : '',
-  assetPrefix: isGhPages ? '/gamenative-landing-page/' : '',
+  basePath: isGhPages ? '/gamenative-landing-page-fix' : '',
+  assetPrefix: isGhPages ? '/gamenative-landing-page-fix/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
